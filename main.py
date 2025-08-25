8204c7f9ac16f8be12d9ff484aed3366ba6a0fcb
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-25 14:57:16 -0700

    print response

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+import os
+# Loads Python’s OS module.
+# Lets your program read or write environment variables (temporary in your process) and do other OS stuff (files, paths, commands).
+from dotenv import load_dotenv
+# Imports the function load_dotenv from the python-dotenv library.
+# This function knows how to read a .env file and put its key-value pairs into your program’s environment memory.
+
+load_dotenv()
+# Actually loads the .env file from your project folder.
+# After this, the keys in .env exist in your program’s temporary environment (not the OS).
+api_key = os.environ.get("GEMINI_API_KEY")
+# Uses os to read the value of GEMINI_API_KEY from the program’s environment.
+# Stores it in the Python variable api_key for your code to use.
+
+from google import genai
+
 def main():
     print("Hello from ai-agent!")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
