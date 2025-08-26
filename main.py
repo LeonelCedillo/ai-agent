fe7890ee2d5d7b65130789079630f64cb0ebc2d6
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-25 17:51:37 -0700

    fix sys.argv

--- main.py
+++ main.py
@@ -14,15 +14,20 @@
 # Stores it in the Python variable api_key for your code to use.
 
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) < 2: 
+        print("Usage: uv run main.py <prompt>")
+        sys.exit(1)
+    prompt = sys.argv[1]
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        model='gemini-2.0-flash-001', contents=prompt
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
