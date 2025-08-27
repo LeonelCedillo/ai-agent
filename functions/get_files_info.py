642323ad4b4b6370cfbc451aed00a6b32810c220
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-26 19:11:07 -0700

    progress

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -9,8 +9,12 @@ def get_files_info(working_directory, directory="."):
     if not os.path.isdir(directory):
         return f'Error: "{directory}" is not a directory'
 
-    contents = os.listdir(directory)
-    for content in contents:
+    contents_list = os.listdir(directory)
+    info_lines = []
+    for content in contents_list:
         size = os.path.getsize(content)
-        is_file = os.path.isfile(content)
-        
+        is_dir = os.path.isdir(content)
+        line = f"- {content}: file_size={size} bytes, is_dir={is_dir}"
+        info_lines.append(line)
+    info_str = "\n".join(info_lines)
+
