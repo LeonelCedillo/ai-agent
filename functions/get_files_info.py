f438869739cc1085ef507c884af36ad0d0491f54
LeonelCedillo <leonel.cs.979@gmail.com>
2025-08-26 19:28:40 -0700

    progress

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,20 +1,22 @@
 import os 
 
 def get_files_info(working_directory, directory="."):
-    full_path = os.path.abspath.join(working_directory, directory)
+    working_directory = os.path.abspath(working_directory)
+    full_path = os.path.abspath(os.path.join(working_directory, directory))
 
     if not full_path.startswith(working_directory):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(directory):
+    if not os.path.isdir(full_path):
         return f'Error: "{directory}" is not a directory'
 
-    contents_list = os.listdir(directory)
+    contents_list = os.listdir(full_path)
     info_lines = []
     for content in contents_list:
-        size = os.path.getsize(content)
-        is_dir = os.path.isdir(content)
+        size = os.path.getsize(os.path.join(full_path, content))
+        is_dir = os.path.isdir(os.path.join(full_path, content))
         line = f"- {content}: file_size={size} bytes, is_dir={is_dir}"
         info_lines.append(line)
-    info_str = "\n".join(info_lines)
+    info = "\n".join(info_lines)
+    return info
 
